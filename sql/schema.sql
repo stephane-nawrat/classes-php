7f84f6168869ed58fba4316dc768bfcf62167b86
stephane-nawrat <stephane.nawrat@laplateforme.io>
2025-10-21 12:12:10 +0200

    feat: add database creation script

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+-- =============================================
+-- Script de création de la base de données
+-- Projet : Classes-PHP
+-- =============================================
+
+-- Création de la base de données
+CREATE DATABASE IF NOT EXISTS phase01_classes 
+CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
+
+-- Sélection de la base
+USE phase01_classes;
